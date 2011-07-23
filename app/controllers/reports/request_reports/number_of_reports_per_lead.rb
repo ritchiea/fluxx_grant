92dcba8c7b8655ecfbef8e900d61c10ff964178d
Andrew Lee <drew@intramedia.net>
2011-07-22 15:37:11 -1000

    FLX-1336 Create a maximum width property for bar charts, set to 50 px globally

--- app/controllers/reports/request_reports/number_of_reports_per_lead.rb
+++ app/controllers/reports/request_reports/number_of_reports_per_lead.rb
@@ -42,9 +42,6 @@ class NumberOfReportsPerLead < ActionController::ReportBase
     hash[:series] = report_types.map{|report_type| {:label => report_type}}
     hash[:stackSeries] = false;
     hash[:type] = "bar"
-    if xaxis.count < 10
-      hash[:seriesDefaults] = {:rendererOptions => {:barPadding => 20}}
-    end
 
     hash.to_json
 
