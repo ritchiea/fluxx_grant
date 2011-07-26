980bf207922f593b2c18b0728ba857543e29b086
Andrew Lee <drew@intramedia.net>
2011-07-26 09:49:07 -1000

    Missing fix

--- app/controllers/reports/modal_reports/budget_overview_by_year.rb
+++ app/controllers/reports/modal_reports/budget_overview_by_year.rb
@@ -54,7 +54,7 @@ class BudgetOverviewByYear < ActionController::ReportBase
 
       hash = {:library => "jqPlot"}
 
-      xaxis.each_index{|i| xaxis[i] += "  #{(((total_granted[i].to_f + pipeline[i].to_f)/ budgeted[i].to_f) * 100).round.to_s}%"}
+      xaxis.each_index{|i| xaxis[i] += "  #{(((total_granted[i].to_f + pipeline[i].to_f)/ budgeted[i].to_f) * 100).round.to_s rescue 0}%"}
       #total_granted.each_index{|i| total_granted[i] -= pipeline[i] }
       budgeted.each_index{|i| budgeted[i] -= (pipeline[i] + total_granted[i]) }
 
