d333cc068a63dbca6f869ec1054ab534cf8b4835
Eric Hansen <ehansen486@gmail.com>
2011-10-18 08:54:35 -0700

    add a way to determine if a fiscal org is specified or not

--- lib/extensions/models/fluxx_request.rb
+++ lib/extensions/models/fluxx_request.rb
@@ -47,7 +47,7 @@ module FluxxRequest
   begin FAR_IN_THE_FUTURE.to_i rescue FAR_IN_THE_FUTURE = Time.now + 10.year end
 
   # for liquid_methods info see: https://github.com/tobi/liquid/blob/master/lib/liquid/module_ex.rb
-  LIQUID_METHODS = [:request_id, :grant_id, :grant_or_request_id, :project_summary, :grant_agreement_at, :grant_begins_at, :grant_ends_at, :request_received_at, :fip_projected_end_at, :amount_requested, :amount_recommended, :duration_in_months, :program_lead, :signatory_contact, :signatory_user_org, :signatory_user_org_title, :address_org, :program, :initiative, :sub_program, :request_transactions, :request_reports, :letter_request_reports, :request_evaluation_metrics, :letter_project_summary_without_leading_to, :first_transaction, :ierf_proposed_end_at, :ierf_budget_end_at, :program_organization, :fiscal_organization, :grantee_org_owner_with_specific, :letter_project_summary, :signatory_contact_title, :request_funding_sources, :budget_requests, :state_to_english, :first_transaction, :board_authority_year, :request_received_at_year ]  
+  LIQUID_METHODS = [:request_id, :grant_id, :grant_or_request_id, :project_summary, :grant_agreement_at, :grant_begins_at, :grant_ends_at, :request_received_at, :fip_projected_end_at, :amount_requested, :amount_recommended, :duration_in_months, :program_lead, :signatory_contact, :signatory_user_org, :signatory_user_org_title, :address_org, :program, :initiative, :sub_program, :request_transactions, :request_reports, :letter_request_reports, :request_evaluation_metrics, :letter_project_summary_without_leading_to, :first_transaction, :ierf_proposed_end_at, :ierf_budget_end_at, :program_organization, :fiscal_organization, :grantee_org_owner_with_specific, :letter_project_summary, :signatory_contact_title, :request_funding_sources, :budget_requests, :state_to_english, :first_transaction, :board_authority_year, :request_received_at_year, :fiscal_sponsor_org ]  
 
   def self.included(base)
     base.send :include, AASM
@@ -1055,6 +1055,9 @@ module FluxxRequest
       if fiscal_organization
         fiscal_org_owner_without_specific
       end
+    end
+    def fiscal_sponsor_org
+      fiscal_organization
     end
     def fiscal_signatory_with_specific
       if fiscal_organization
