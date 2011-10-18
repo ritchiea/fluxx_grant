3f43ce5d1b3e7cb344425f332aee865c636afea3
Eric Hansen <ehansen486@gmail.com>
2011-10-18 16:33:18 -0700

    add a way to populate the old_amount_recommended, etc. values

--- lib/generators/fluxx_grant_migration/templates/add_old_value_fields_to_request_amendments_table.rb
+++ lib/generators/fluxx_grant_migration/templates/add_old_value_fields_to_request_amendments_table.rb
@@ -6,6 +6,23 @@ class FluxxGrantAddOldValueFieldsToRequestAmendmentsTable < ActiveRecord::Migrat
       t.datetime :old_end_date
       t.decimal :old_amount_recommended, :scale => 2, :precision => 15
     end
+
+    execute "create temporary table original_amendment_values select request_amendments.id, ra_orig.amount_recommended old_amount_recommended,
+            ra_orig.duration old_duration,
+            ra_orig.start_date old_start_date,
+            ra_orig.end_date old_end_date
+
+            from request_amendments
+            LEFT OUTER JOIN request_amendments ra_orig on ra_orig.request_id = request_amendments.request_id and ra_orig.original = 1
+            where request_amendments.original <> 1"
+
+
+      execute "update request_amendments ra, original_amendment_values ora
+      set ra.old_amount_recommended = ora.old_amount_recommended,
+      ra.old_duration = ora.old_duration,
+      ra.old_start_date = ora.old_start_date,
+      ra.old_end_date = ora.old_end_date
+      where ra.id = ora.id"
   end
 
   def self.down
