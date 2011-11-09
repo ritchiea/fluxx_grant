f23b06569256d9e49ef021f9af1560f3d3b936d1
Eric Hansen <ehansen486@gmail.com>
2011-11-09 13:00:39 -0800

    shuffle around some files

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,4 @@ Rails.application.routes.draw do
   resources :grantee_portal
   resources :portal_grant_requests
 
-  match 'login' => 'user_sessions#new', :as => :login
-  match 'logout' => 'user_sessions#destroy', :as => :logout
-  match 'portal', :controller => :user_sessions
 end
