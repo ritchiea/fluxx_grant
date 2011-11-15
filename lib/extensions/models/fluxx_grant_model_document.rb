6a360a31b636c575924aecd3cc3eaaa35ea9e753
Eric Hansen <ehansen486@gmail.com>
2011-11-15 13:30:06 -0800

    get rid of an errant override of model_document

--- lib/extensions/models/fluxx_grant_model_document.rb
+++ lib/extensions/models/fluxx_grant_model_document.rb
@@ -11,6 +11,18 @@ module FluxxGrantModelDocument
       doc_label = options[:doc_label]
       Fluxx.config("documents_allowed_#{doc_label}_#{user && user.user_profile && user.user_profile.name}")
     end
+    
+    def relates_to_user? user
+      if (self.documentable.class.name == 'RequestReport')
+        (user.primary_organization.id == self.documentable.request.program_organization_id) || (user.primary_organization.id == self.documentable.request.fiscal_organization_id)
+      else
+        if (current_user.is_portal_user?)
+          self.created_by.is_portal_user? if self.created_by
+        else
+          false
+        end
+      end
+    end
   end
   
   instance_methods do
