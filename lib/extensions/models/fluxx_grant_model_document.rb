0e0107e7a7f27a513eedf689570b199815429fab
Eric Hansen <ehansen486@gmail.com>
2011-12-09 19:11:04 -0800

    correct issue with relates_to_user

--- lib/extensions/models/fluxx_grant_model_document.rb
+++ lib/extensions/models/fluxx_grant_model_document.rb
@@ -16,7 +16,7 @@ module FluxxGrantModelDocument
       if (self.documentable.class.name == 'RequestReport')
         (user.primary_organization.id == self.documentable.request.program_organization_id) || (user.primary_organization.id == self.documentable.request.fiscal_organization_id)
       else
-        if (current_user.is_portal_user?)
+        if (user.is_portal_user?)
           self.created_by.is_portal_user? if self.created_by
         else
           false
