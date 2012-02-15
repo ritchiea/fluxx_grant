3e2fe1facb7f13dd2b3e12ed1edbccb817aa715a
Eric Hansen <ehansen486@gmail.com>
2012-02-15 09:33:54 -0800

    reswizzle the reviewer export and feedback reports

--- app/controllers/reports/modal_reports/reviewer_export_report.rb
+++ app/controllers/reports/modal_reports/reviewer_export_report.rb
@@ -11,6 +11,82 @@ class ReviewerExportReport < ActionController::ReportBase
   end
 
   def compute_document_data controller, show_object, params, report_vars, models
-    base_compute_show_document_data controller, show_object, params, report_vars, :export
-  end
-end
+      start_date, end_date, reviews_by_request_id, requests_by_requestid, assigned_users, assignments, reviews, user_ids, request_ids, users, users_by_userid, requests = base_compute_show_document_data(controller, show_object, params, report_vars)
+      output = StringIO.new
+
+      workbook = WriteExcel.new(output)
+      worksheet = workbook.add_worksheet
+
+      non_wrap_bold_format, bold_format, header_format, solid_black_format, amount_format, number_format, date_format, text_format, header_format, 
+          sub_total_format, sub_total_border_format, total_format, total_border_format, final_total_format, final_total_border_format, 
+          bold_total_format, double_total_format = build_formats(workbook)
+      # Add page summary
+      # worksheet.write(0, 0, 'The Energy Foundation', non_wrap_bold_format)
+      worksheet.write(1, 0, 'Reviewer Feedback', non_wrap_bold_format)
+      worksheet.write(2, 0, 'Start Date: ' + start_date.mdy) if start_date
+      worksheet.write(3, 0, 'End Date: ' + end_date.mdy) if end_date
+      worksheet.write(4, 0, "Report Date: " + Time.now.mdy)
+
+      # Adjust column widths
+      worksheet.set_column(0, 9, 10)
+      worksheet.set_column(1, 1, 15)
+      worksheet.set_column(7, 7, 20)
+      worksheet.set_column(9, 9, 15)
+      column_letters = calculate_column_letters
+
+      row_start = 6
+      row = row_start
+
+      program_hash = Program.all.inject({}) {|acc, program| acc[program.id] = program; acc}
+      sub_program_hash = SubProgram.all.inject({}) {|acc, program| acc[program.id] = program; acc}
+
+      column_headers = ["Grant Name", "Grant ID", I18n.t(:program_name), I18n.t(:sub_program_name), "Amount Requested", "Amount Recommended", "Start Date", "End Date", "Duration", "Reviewer Name", "Rating", "Review Type", "Comment", "Benefits", "Outcomes", "Merits", "Recommendation"]
+      if Fluxx.config(:dont_use_duration_in_requests) == "1"
+        column_headers.delete "Duration"
+      end
+
+      column_headers.each_with_index{|label, index| worksheet.write(6, index, label, header_format)}
+
+      reviews.each do |review|
+        request_id = review.request_id
+        column=0
+        request = requests_by_requestid[request_id]
+
+        worksheet.write(row += 1, column, request.report_grant_name)
+        worksheet.write(row, column += 1, request.base_request_id)
+        program = program_hash[request.program_id]
+        worksheet.write(row, column += 1, program ? program.name : nil)
+        sub_program = sub_program_hash[request.sub_program_id]
+        worksheet.write(row, column += 1, sub_program ? sub_program.name : nil)
+        worksheet.write(row, column += 1, (request.amount_requested), amount_format)
+        worksheet.write(row, column += 1, (request.amount_recommended), amount_format)
+        worksheet.write(row, column += 1, (request.report_begin_date ? request.report_begin_date.mdy : ''), date_format)
+
+        if Fluxx.config(:dont_use_duration_in_requests) == "1"
+          if request.is_a?(FipRequest)
+            worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
+          else
+            worksheet.write(row, column += 1, (request.grant_closed_at ? request.grant_closed_at.mdy : ''), date_format)
+          end
+        elsif request.is_a?(FipRequest)
+          worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
+          column += 1 # Fips still have a duration column
+        else
+          worksheet.write(row, column += 1, (request.report_end_date ? request.report_end_date.mdy : ''), date_format)
+          worksheet.write(row, column += 1, request.duration_in_months, number_format)
+        end
+
+        user = users_by_userid[review.created_by_id]
+        worksheet.write(row, column += 1, user ? user.full_name : nil)
+        worksheet.write(row, column += 1, review.rating, number_format)
+        worksheet.write(row, column += 1, review.review_type)
+        worksheet.write(row, column += 1, review.comment)
+        worksheet.write(row, column += 1, review.benefits)
+        worksheet.write(row, column += 1, review.outcomes)
+        worksheet.write(row, column += 1, review.merits)
+        worksheet.write(row, column += 1, review.recommendation)
+      end
+
+      workbook.close
+      output.string
+    end
--- lib/extensions/reports/reviewer_base_report.rb
+++ lib/extensions/reports/reviewer_base_report.rb
@@ -1,5 +1,5 @@
 module ReviewerBaseReport
-  def base_compute_show_document_data controller, show_object, params, report_vars, report_type=:export
+  def base_compute_show_document_data controller, show_object, params, report_vars, extra_conditions=nil
     active_record_params = params[:active_record_base] || {}
 
     start_date = if active_record_params[:start_date].blank?
@@ -24,6 +24,21 @@ module ReviewerBaseReport
       User.where(:id => active_record_params[:lead_user_ids]).all rescue nil
     end || []
     lead_users = lead_users.compact
+    
+    assignments = RequestReviewerAssignment.joins(:request).where([%{
+      #{start_date ? " request_received_at >= '#{start_date.sql}' AND " : ''} 
+      #{end_date ? " request_received_at <= '#{end_date.sql}' AND " : ''}
+      requests.deleted_at IS NULL AND 
+      granted = 0 AND
+      requests.state not in (?) AND
+      (1=? or requests.program_id in (?)) AND
+      (1=? or requests.program_lead_id in (?))}, 
+      Request.all_rejected_states,
+      programs.empty?, programs,
+      lead_users.empty?, lead_users
+      ]).all
+      
+    assigned_users = User.where(:id => assignments.map(&:user_id)).all
 
     reviews = 
       RequestReview.joins(:request).where([%{
@@ -38,17 +53,17 @@ module ReviewerBaseReport
         programs.empty?, programs,
         lead_users.empty?, lead_users
         ])
-    reviews = reviews.where(:review_type => 'HRAC Review') if report_type == :feedback
+    reviews = reviews.where(extra_conditions) if extra_conditions
     user_ids = reviews.map(&:created_by_id).uniq
     request_ids = reviews.map(&:request_id).uniq
     users = User.where(:id => user_ids).order('last_name, first_name').all
     users_by_userid = users.inject({}) {|acc, user| acc[user.id] = user; acc}
     requests = Request.find_by_sql ["
-    select requests.*, if(type = 'GrantRequest', (select name from organizations where id = program_organization_id), fip_title) report_grant_name,
-    grant_begins_at report_begin_date,
-    if(grant_begins_at is not null and duration_in_months is not null, date_add(date_add(grant_begins_at, INTERVAL duration_in_months month), interval -1 DAY), grant_begins_at) report_end_date
-    from requests 
-    where id in (?)", request_ids]
+      select requests.*, if(type = 'GrantRequest', (select name from organizations where id = program_organization_id), fip_title) report_grant_name,
+      grant_begins_at report_begin_date,
+      if(grant_begins_at is not null and duration_in_months is not null, date_add(date_add(grant_begins_at, INTERVAL duration_in_months month), interval -1 DAY), grant_begins_at) report_end_date
+      from requests 
+      where id in (?)", request_ids]
     requests_by_requestid = requests.inject({}) {|acc, request| acc[request.id] = request; acc}
 
     reviews_by_request_id = {}
@@ -59,142 +74,7 @@ module ReviewerBaseReport
       acc
     end
 
-    output = StringIO.new
+    [start_date, end_date, reviews_by_request_id, requests_by_requestid, assigned_users, assignments, reviews, user_ids, request_ids, users, users_by_userid, requests]
 
-    workbook = WriteExcel.new(output)
-    worksheet = workbook.add_worksheet
-
-    non_wrap_bold_format, bold_format, header_format, solid_black_format, amount_format, number_format, date_format, text_format, header_format, 
-        sub_total_format, sub_total_border_format, total_format, total_border_format, final_total_format, final_total_border_format, 
-        bold_total_format, double_total_format = build_formats(workbook)
-    # Add page summary
-    # worksheet.write(0, 0, 'The Energy Foundation', non_wrap_bold_format)
-    worksheet.write(1, 0, 'Reviewer Feedback', non_wrap_bold_format)
-    worksheet.write(2, 0, 'Start Date: ' + start_date.mdy) if start_date
-    worksheet.write(3, 0, 'End Date: ' + end_date.mdy) if end_date
-    worksheet.write(4, 0, "Report Date: " + Time.now.mdy)
-  
-    # Adjust column widths
-    worksheet.set_column(0, 9, 10)
-    worksheet.set_column(1, 1, 15)
-    worksheet.set_column(7, 7, 20)
-    worksheet.set_column(9, 9, 15)
-    column_letters = calculate_column_letters
-  
-  
-    row_start = 6
-    row = row_start
-
-    program_hash = Program.all.inject({}) {|acc, program| acc[program.id] = program; acc}
-    sub_program_hash = SubProgram.all.inject({}) {|acc, program| acc[program.id] = program; acc}
-
-    if report_type == :feedback
-      column_headers = ["Grant Name", "Grant ID", I18n.t(:program_name), I18n.t(:sub_program_name), "Amount Requested", "Amount Recommended", "Start Date", "End Date"]
-      unless Fluxx.config(:dont_use_duration_in_requests) == "1"
-        column_headers << "Duration"
-      end
-      
-      column_headers.each_with_index{|label, index| worksheet.write(6, index, label, header_format)}
-
-      column_offset = column_headers.size
-      users.each_with_index do |user, index|
-        worksheet.write(6, index + column_offset, user.first_name + ' ' + user.last_name, header_format)
-      end
-      worksheet.write(6, users.size + column_offset, 'Average', header_format)
-
-      request_ids.each do |request_id|
-        column=0
-        request = requests_by_requestid[request_id]
-    
-        worksheet.write(row += 1, column, request.report_grant_name)
-        worksheet.write(row, column += 1, request.base_request_id)
-        program = program_hash[request.program_id]
-        worksheet.write(row, column += 1, program ? program.name : nil)
-        sub_program = sub_program_hash[request.sub_program_id]
-        worksheet.write(row, column += 1, sub_program ? sub_program.name : nil)
-        worksheet.write(row, column += 1, (request.amount_requested.to_i rescue 0), amount_format)
-        worksheet.write(row, column += 1, (request.amount_recommended.to_i rescue 0), amount_format)
-        worksheet.write(row, column += 1, (request.report_begin_date ? request.report_begin_date.mdy : ''), date_format)
-        if Fluxx.config(:dont_use_duration_in_requests) == "1"
-          if request.is_a?(FipRequest)
-            worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
-          else
-            worksheet.write(row, column += 1, (request.grant_closed_at ? request.grant_closed_at.mdy : ''), date_format)
-          end
-        elsif request.is_a?(FipRequest)
-          worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
-          column += 1 # Fips still have a duration column
-        else
-          worksheet.write(row, column += 1, (request.report_end_date ? request.report_end_date.mdy : ''), date_format)
-          worksheet.write(row, column += 1, request.duration_in_months, number_format)
-        end
-    
-        start_user_column = column + 1
-        users.each do |user|
-          review = reviews_by_request_id[request_id]
-          user_review = review[user.id] if review
-          if !user_review.rating.blank?
-            worksheet.write(row, column += 1, (user_review ? (user_review.rating.to_i rescue '') : ''), number_format)
-          else
-            column += 1
-          end
-        end
-        end_user_column = column
-    
-        avg_formula = "#{column_letters[start_user_column]}#{row+1}:#{column_letters[end_user_column]}#{row+1}"
-        worksheet.write(row, column+=1, ("=AVERAGE(#{avg_formula})"), number_format)
-      end
-    elsif report_type == :export
-      column_headers = ["Grant Name", "Grant ID", I18n.t(:program_name), I18n.t(:sub_program_name), "Amount Requested", "Amount Recommended", "Start Date", "End Date", "Duration", "Reviewer Name", "Rating", "Review Type", "Comment", "Benefits", "Outcomes", "Merits", "Recommendation"]
-      if Fluxx.config(:dont_use_duration_in_requests) == "1"
-        column_headers.delete "Duration"
-      end
-      
-      column_headers.each_with_index{|label, index| worksheet.write(6, index, label, header_format)}
-      
-      reviews.each do |review|
-        request_id = review.request_id
-        column=0
-        request = requests_by_requestid[request_id]
-
-        worksheet.write(row += 1, column, request.report_grant_name)
-        worksheet.write(row, column += 1, request.base_request_id)
-        program = program_hash[request.program_id]
-        worksheet.write(row, column += 1, program ? program.name : nil)
-        sub_program = sub_program_hash[request.sub_program_id]
-        worksheet.write(row, column += 1, sub_program ? sub_program.name : nil)
-        worksheet.write(row, column += 1, (request.amount_requested), amount_format)
-        worksheet.write(row, column += 1, (request.amount_recommended), amount_format)
-        worksheet.write(row, column += 1, (request.report_begin_date ? request.report_begin_date.mdy : ''), date_format)
-        
-        if Fluxx.config(:dont_use_duration_in_requests) == "1"
-          if request.is_a?(FipRequest)
-            worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
-          else
-            worksheet.write(row, column += 1, (request.grant_closed_at ? request.grant_closed_at.mdy : ''), date_format)
-          end
-        elsif request.is_a?(FipRequest)
-          worksheet.write(row, column += 1, (request.fip_projected_end_at ? request.fip_projected_end_at.mdy : ''), date_format)
-          column += 1 # Fips still have a duration column
-        else
-          worksheet.write(row, column += 1, (request.report_end_date ? request.report_end_date.mdy : ''), date_format)
-          worksheet.write(row, column += 1, request.duration_in_months, number_format)
-        end
-        
-        user = users_by_userid[review.created_by_id]
-        worksheet.write(row, column += 1, user ? user.full_name : nil)
-        worksheet.write(row, column += 1, review.rating, number_format)
-        worksheet.write(row, column += 1, review.review_type)
-        worksheet.write(row, column += 1, review.comment)
-        worksheet.write(row, column += 1, review.benefits)
-        worksheet.write(row, column += 1, review.outcomes)
-        worksheet.write(row, column += 1, review.merits)
-        worksheet.write(row, column += 1, review.recommendation)
-        
-      end
-    end  
-
-    workbook.close
-    output.string
   end
 end
