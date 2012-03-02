e8d1d4c11d6be34c7f0aa78c81109a3032ac94bc
Mark Johnson <mark@logicspin.com>
2012-03-02 07:19:11 +0700

    stub geocoder in tests

--- test/unit/models/organization_test.rb
+++ test/unit/models/organization_test.rb
@@ -8,4 +8,14 @@ class OrganizationTest < ActiveSupport::TestCase
   test "test creating organization" do
     assert @org.id
   end
+  
+  test "geocoding sets latitude and longitude post validation" do
+    @org.postal_code = '96734'
+    assert_nil @org.latitude
+    assert_nil @org.longitude
+    @org.valid?
+    assert_equal 1.0, @org.latitude  # values from stubbed geocoder
+    assert_equal 2.0, @org.longitude
+  end
+  
 end
